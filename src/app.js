4443e74be532d26a05ef1aaa1335dc6803017e4f
R3dH4wk44 <109897647+R3dH4wk44@users.noreply.github.com>
2024-01-06 13:18:26 +0000

    Primera Version

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const who = ["The dog", "My grandma", "His turtle", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "the keys", "the car"];
+  const when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generarExcusa() {
+    console.log(Math.floor(Math.random() * 4));
+    let excusa =
+      who[Math.floor(Math.random() * 4)] +
+      " " +
+      action[Math.floor(Math.random() * 4)] +
+      " " +
+      what[Math.floor(Math.random() * 3)] +
+      " " +
+      when[Math.floor(Math.random() * 5)];
+
+    document.getElementById("excusas").innerHTML = excusa;
+  }
+
+  document.getElementById("generar").addEventListener("click", generarExcusa);
 };
